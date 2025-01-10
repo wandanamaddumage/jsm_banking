20b522468a8bb8b47c9c0bfdf570f1e857ec7754
Wandana Mddumage <wandanamuthumali99@gmail.com>
2025-01-10 05:54:48 +0530

    update: home page

---
+++ components/HeaderBox.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const HeaderBox = ({
+  type = "title",
+  title,
+  subtext,
+  user,
+}: HeaderBoxProps) => {
+  return (
+    <div className="header-box">
+      <h1 className="header-box-title">
+        {title}
+        {type === "greeting" && (
+          <span className="text-bankGradient">&nbsp;{user}</span>
+        )}
+      </h1>
+      <div className="header-box-subtext">{subtext}</div>
+    </div>
+  );
+};
+
+export default HeaderBox;
