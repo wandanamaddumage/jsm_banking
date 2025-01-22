0a26d007807888269b1c1abc2d4c8ac75e10c61d
Wandana Mddumage <wandanamuthumali99@gmail.com>
2025-01-22 15:16:37 +0530

    feat: add sign in form

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,10 @@
 "use server";
 
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
+
 export const signIn = async () => {
   try {
     // Mutation / Database / Make fetch
@@ -9,9 +14,39 @@ export const signIn = async () => {
 };
 
 export const signUp = async (userData: SignUpParams) => {
+  const { email, password, firstName, lastName } = userData;
+
   try {
-    // Create a user account
+    const { account } = await createAdminClient();
+
+    const newUserAccount = await account.create(
+      ID.unique(),
+      userData.email,
+      userData.password,
+      `${firstName} ${lastName}`
+    );
+    const session = await account.createEmailPasswordSession(email, password);
+
+    (await cookies()).set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+
+    return parseStringify(newUserAccount);
   } catch (error) {
     console.error("Error", error);
   }
 };
+
+// ... your initilization functions
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    return await account.get();
+  } catch (error) {
+    return null;
+  }
+}
