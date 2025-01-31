b2de64c3d8ef960fad03eb12997ab62027f527df
Wandana Mddumage <wandanamuthumali99@gmail.com>
2025-01-31 20:22:10 +0530

    feat: Implement auth, appwrite, and sentry.

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,10 +1,25 @@
+import { useCallback, useEffect, useState } from "react";
 import { Button } from "./ui/button";
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const getLinkToken = async () => {
+      //    const data = await createLinkToken(user)
+    };
+    getLinkToken;
+  }, []);
+  const onSuccess = useCallback(async () => {}, [user]);
+
+  const config: PlaidLinkOptions = {
+    token,
+    onSuccess,
+  };
   return (
     <>
       {variant === "primary" ? (
-        <Button>Connect Bank</Button>
+        <Button className="plaidlink-primary">Connect Bank</Button>
       ) : variant === "ghost" ? (
         <Button>Connect bank</Button>
       ) : (
