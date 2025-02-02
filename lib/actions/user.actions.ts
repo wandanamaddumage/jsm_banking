2c2a44ffd70eb0b83a8cce967cdfb8d1bc4a5ffc
Wandana Mddumage <wandanamuthumali99@gmail.com>
2025-02-02 13:41:18 +0530

    feat: Implement auth, appwrite, and sentry.

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,8 @@ import { ID } from "node-appwrite";
 import { createAdminClient, createSessionClient } from "../appwrite";
 import { cookies } from "next/headers";
 import { parseStringify } from "../utils";
+import { plaidClient } from "../plaid";
+import { CountryCode, Products } from "plaid";
 
 export const signIn = async ({ email, password }: signInProps) => {
   try {
@@ -76,5 +78,17 @@ export const logoutAccount = async () => {
 
 export const createLinkToken = async (user: User) => {
   try {
+    const tokenParams = {
+      user: {
+        client_user_id: user.$id,
+      },
+      client_name: user.name,
+      products: ["auth"] as Products[],
+      language: "en",
+      country_codes: ["US"] as CountryCode[],
+    };
+
+    const response = await plaidClient.linkTokenCreate(tokenParams);
+    return parseStringify({ linkToken: response.data.link_token });
   } catch (error) {}
 };
