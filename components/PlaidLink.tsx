bcb08363c4d5a35cd0af7db0952f9f1a9c512e94
Wandana Mddumage <wandanamuthumali99@gmail.com>
2025-03-12 14:21:13 +0530

    Implement Plaid link and Dwolla

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,29 +1,65 @@
 import { useCallback, useEffect, useState } from "react";
 import { Button } from "./ui/button";
+import { StyledString } from "next/dist/build/swc/types";
+import { useRouter } from "next/navigation";
+import {
+  PlaidLinkOnSuccess,
+  PlaidLinkOptions,
+  usePlaidLink,
+} from "react-plaid-link"; // Ensure this import is correct based on your project setup
+import {
+  createLinkToken,
+  exchangePublicToken,
+} from "@/lib/actions/user.actions";
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
+  const router = useRouter();
   const [token, setToken] = useState("");
 
   useEffect(() => {
     const getLinkToken = async () => {
-      //    const data = await createLinkToken(user)
+      const data = await createLinkToken(user);
+      setToken(data?.linkToken);
     };
-    getLinkToken;
+    getLinkToken();
   }, []);
-  const onSuccess = useCallback(async () => {}, [user]);
+
+  const onSuccess = useCallback<PlaidLinkOnSuccess>(
+    async (public_token: String) => {
+      await exchangePublicToken({
+        publicToken: public_token,
+        user,
+      });
+      router.push("/");
+    },
+    [user]
+  );
 
   const config: PlaidLinkOptions = {
     token,
     onSuccess,
   };
+
+  const { open, ready } = usePlaidLink(config);
+
   return (
     <>
       {variant === "primary" ? (
-        <Button className="plaidlink-primary">Connect Bank</Button>
+        <Button
+          className="plaidlink-primary"
+          onClick={() => open()}
+          disabled={!ready}
+        >
+          Connect Bank
+        </Button>
       ) : variant === "ghost" ? (
-        <Button>Connect bank</Button>
+        <Button onClick={() => open()} disabled={!ready}>
+          Connect bank
+        </Button>
       ) : (
-        <Button>Connect Bank</Button>
+        <Button onClick={() => open()} disabled={!ready}>
+          Connect Bank
+        </Button>
       )}
     </>
   );
